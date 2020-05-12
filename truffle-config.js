c8c23407b30c556fb4b820eaa7794eb7758c7f92
kreloaded <kiran@ost.com>
2020-05-12 10:59:38 +0530

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,11 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
-    }
-  }
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // match any network
+      websockets: true
+    },
+  },
 };
