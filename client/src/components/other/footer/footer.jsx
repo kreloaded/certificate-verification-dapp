c9fac7acf83d9211872fbda2f233e0d374db7cc5
kreloaded <kiran@ost.com>
2020-05-17 20:03:37 +0530

    Updated footer for className

--- client/src/components/other/footer/footer.jsx
+++ client/src/components/other/footer/footer.jsx
@@ -4,8 +4,8 @@ class Footer extends Component {
     render () {
         return (
             <div>
-                <footer class="section footer-classic context-light bg-image" style={{background: "#F0F8FF"}}>
-                    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                <footer className="section footer-classic context-light bg-image" style={{background: "#F0F8FF"}}>
+                    <div className="footer-copyright text-center py-3">© 2020 Copyright:
                         <a href="https://github.com/kreloaded" target="_blank"> Visit Github</a>
                     </div>
                 </footer>
