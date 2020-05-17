a71bf01c649fa1056ab3160428d4b510b4eba395
kreloaded <kiran@ost.com>
2020-05-17 21:45:03 +0530

    Added css for failed error message

--- client/src/components/certificate/view/ViewCertificate.jsx
+++ client/src/components/certificate/view/ViewCertificate.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import web3 from '../../../getWeb3';
 import CertificateContract from '../../../contracts/Certificate.json';
+import FailedBlockchain from '../../other/error/Failed';
 
 import './ViewCertificate.css';
 
@@ -14,13 +15,14 @@ class ViewCertificate extends Component {
             web3: null,
             contract: null,
             account: null,
-            isFetched: false,
             fname: '',
             lname: '',
             courseName: '',
             issuingAuthority: '',
             issueDate: '',
             fullName: '',
+            isFetched: false,
+            isConnected: false,
         }
 
         this.loadBlockchain = this.loadBlockchain.bind(this);
@@ -33,21 +35,32 @@ class ViewCertificate extends Component {
     }
 
     async loadBlockchain() {
-        const accounts = await web3.eth.getAccounts();
-        console.log('accounts :-', accounts);
+        console.log('web3 provider ::::', await web3.givenProvider);
+        const givenProvider = await web3.givenProvider;
 
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = CertificateContract.networks[networkId];
-        const instance = new web3.eth.Contract(
-            CertificateContract.abi,
-            deployedNetwork && deployedNetwork.address,
-        );
+        if(givenProvider !== null) {
+            this.setState({
+                isConnected: true,
+            });
+        }
 
-        this.setState({
-            web3,
-            account: accounts[0],
-            contract: instance
-        });
+        if(this.state.isConnected) {
+            const accounts = await web3.eth.getAccounts();
+            console.log('accounts :-', accounts);
+
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = CertificateContract.networks[networkId];
+            const instance = new web3.eth.Contract(
+                CertificateContract.abi,
+                deployedNetwork && deployedNetwork.address,
+            );
+
+            this.setState({
+                web3,
+                account: accounts[0],
+                contract: instance
+            });
+        }
     }
 
     handleChange(event) {
@@ -89,50 +102,56 @@ class ViewCertificate extends Component {
     }
 
     render () {
-        return (
-            <div className="view-certificate">
-                <form className="form">
-                    <div className="form-group row">
-                        <label htmlFor="certificateId" className="col-sm-4 col-form-label">Enter Certificate ID: </label>
-                        <div className="col-sm-6">
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="certificateId"
-                                value={this.state.certificateId}
-                                onChange={this.handleChange} />
+        if(!this.state.isConnected) {
+            return (
+                <FailedBlockchain />
+            );
+        } else {
+            return (
+                <div className="view-certificate">
+                    <form className="form">
+                        <div className="form-group row">
+                            <label htmlFor="certificateId" className="col-sm-4 col-form-label">Enter Certificate ID: </label>
+                            <div className="col-sm-6">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    name="certificateId"
+                                    value={this.state.certificateId}
+                                    onChange={this.handleChange} />
+                            </div>
                         </div>
+                    </form>
+                    <div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                            onClick={this.handleSubmit}>
+                                Get Certificate
+                        </button>
                     </div>
-                </form>
-                <div>
-                    <button
-                        type="submit"
-                        className="btn btn-primary"
-                        onClick={this.handleSubmit}>
-                            Get Certificate
-                    </button>
+                    <div className="certificate-details">
+                        {this.state.isFetched ?
+                            <div className="details">
+                                <h2 className="title">Certificate Details</h2>
+                                <dl className="dl-horizontal row">
+                                    <dt className="col-4">Full Name: </dt>
+                                    <dd className="col-8">{this.state.fullName}</dd>
+                                    <dt className="col-4">Certificate ID: </dt>
+                                    <dd className="col-8">{this.state.certificateId}</dd>
+                                    <dt className="col-4">Course Name: </dt>
+                                    <dd className="col-8">{this.state.courseName}</dd>
+                                    <dt className="col-4">Issuing Authority: </dt>
+                                    <dd className="col-8">{this.state.issuingAuthority}</dd>
+                                    <dt className="col-4">Issue Date: </dt>
+                                    <dd className="col-8">{this.state.issueDate}</dd>
+                                </dl>
+                            </div>
+                             : null}
+                    </div>
                 </div>
-                <div className="certificate-details">
-                    {this.state.isFetched ?
-                        <div className="details">
-                            <h2 className="title">Certificate Details</h2>
-                            <dl className="dl-horizontal row">
-                                <dt className="col-4">Full Name: </dt>
-                                <dd className="col-8">{this.state.fullName}</dd>
-                                <dt className="col-4">Certificate ID: </dt>
-                                <dd className="col-8">{this.state.certificateId}</dd>
-                                <dt className="col-4">Course Name: </dt>
-                                <dd className="col-8">{this.state.courseName}</dd>
-                                <dt className="col-4">Issuing Authority: </dt>
-                                <dd className="col-8">{this.state.issuingAuthority}</dd>
-                                <dt className="col-4">Issue Date: </dt>
-                                <dd className="col-8">{this.state.issueDate}</dd>
-                            </dl>
-                        </div>
-                         : null}
-                </div>
-            </div>
-        );
+            );
+        }
     };
 };
 
--- client/src/components/other/error/Failed.jsx
+++ client/src/components/other/error/Failed.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import './Failed.css';
+
 class Failed extends Component {
     render () {
         return (
-            <h1>Failed Loading Blockchain !</h1>
+            <div className="failed">
+                <h2>Loading Blockchain Failed ! </h2>
+                <h3>Currently only connection through metamask is supported.</h3>
+                <h3>Please check your metamask is active and connected ! </h3>
+            </div>
         );
     };
 };
