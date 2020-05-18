048e7b3ec5b37d2cdadb32a80f45a65dc9196cb0
kreloaded <kiran@ost.com>
2020-05-18 18:41:13 +0530

    Added page not found support

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/other/Navbar';
 import Home from './components/other/Home/Home';
 import AddCertificate from './components/certificate/add/AddCertificate';
 import ViewCertificate from './components/certificate/view/ViewCertificate';
 import GetTransactionDetails from './components/certificate/transaction/GetTransactionDetails';
+import Page404 from './components/other/error/page-not-found/Page404';
 import Footer from './components/other/footer/Footer';
 
 class Routes extends Component {
@@ -13,10 +14,13 @@ class Routes extends Component {
     return (
       <BrowserRouter>
         <Navbar />
-        <Route exact path="/" component={ Home } />
-        <Route path="/add-certificate" component={ AddCertificate } />
-        <Route path="/view-certificate" component={ ViewCertificate } />
-        <Route path="/get-transaction-details" component={ GetTransactionDetails } />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/add-certificate" component={ AddCertificate } />
+          <Route path="/view-certificate" component={ ViewCertificate } />
+          <Route path="/get-transaction-details" component={ GetTransactionDetails } />
+          <Route component={ Page404 } />
+        </Switch>
         <Footer />
       </BrowserRouter>
     );
---
+++ client/src/components/other/error/page-not-found/Page404.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Page404 extends Component {
+    render () {
+        return (
+            <div style={{marginTop: "50px"}}>
+                <h2>Page not found !</h2>
+            </div>
+        );
+    };
+};
+
+export default Page404;
